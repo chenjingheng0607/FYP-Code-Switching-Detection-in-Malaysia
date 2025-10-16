0db983a1b5c718478083ed39cdc1591f4272cc54
chenjingheng0607 <yowinchen2@gmail.com>
2025-10-16 14:53:54 +0800

    remove django

--- chat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def chat_interface(request):
-    """
-    Renders the chat interface page.
-    """
-    return render(request, 'chat/chat_interface.html')
